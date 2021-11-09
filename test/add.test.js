d5025a4a1c9d61973823ea542b3abbbdc20b33cd
Milla <milla.valio@tuni.fi>
2021-11-09 13:29:17 +0200

    Update to use Node.Js 14

--- test/add.test.js
+++ test/add.test.js
@@ -1,9 +1,9 @@
 // First test - borrowed from EX1
-//import chai from "chai"
-//import sum from "../sum.js"
+import chai from "chai"
+import add from "../../COMP.SE.200-2021-2022-1/src/add.js"
 // below for node 10
-var chai = require("chai")
-var add = require ("../../COMP.SE.200-2021-2022-1/src/add.js")
+//var chai = require("chai")
+//var add = require ("../../COMP.SE.200-2021-2022-1/src/add.js")
 const expect = chai.expect
 
 
