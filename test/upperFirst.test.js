3ddb1222a428d6b43db9342740d1bc7d66c1df0d
Tuija Palovuori <tuija.palovuori@tuni.fi>
2021-11-11 15:52:33 +0200

    added rest of upperFirst tests

--- test/upperFirst.test.js
+++ test/upperFirst.test.js
@@ -3,7 +3,28 @@ import upperFirst from "../COMP.SE.200-2021-2022-1/src/upperFirst.js"
 const expect = chai.expect
 
 describe("upperFirst", () => {
- it("returns all caps string as is", () =>{
-     expect(upperFirst("TESTING")).to.equal("TESTING");
+ it("returns single uppercase letter as is", () =>{
+     expect(upperFirst("A")).to.equal("A");
+ });
+ it("returns single lowercase letter as its uppercase counterpart", () =>{
+     expect(upperFirst("a")).to.equal("A");
+ });
+ it("returns lowercase multi-character string with first character capitalized and rest unchanged", () =>{
+     expect(upperFirst("testing lowercase")).to.equal("Testing lowercase");
+ });
+ it("returns already capitalized multi-character string as is", () =>{
+     expect(upperFirst("Testing already capitalized")).to.equal("Testing already capitalized");
+ });
+ it("returns all caps multi-character string as is", () =>{
+     expect(upperFirst("TESTING ALL CAPS")).to.equal("TESTING ALL CAPS");
+ });
+ it("returns string with non-letter first character as is", () =>{
+     expect(upperFirst("!testing non-letter first character")).to.equal("!testing non-letter first character");
+ });
+ it("returns string starting with white space as is", () =>{
+     expect(upperFirst(" testing whitespace")).to.equal(" testing whitespace");
+ });
+ it("returns empty string as is", () =>{
+     expect(upperFirst("")).to.equal("");
  });
 })
