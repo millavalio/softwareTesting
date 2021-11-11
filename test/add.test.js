042ebea0318cc038db83cd724d188894b5c1b764
Milla <milla.valio@tuni.fi>
2021-11-11 15:20:06 +0200

    Fix file path

--- test/add.test.js
+++ test/add.test.js
@@ -3,7 +3,7 @@ import chai from "chai"
 import add from "../../COMP.SE.200-2021-2022-1/src/add.js"
 // below for node 10
 //var chai = require("chai")
-//var add = require ("../../COMP.SE.200-2021-2022-1/src/add.js")
+//var add = require ("../COMP.SE.200-2021-2022-1/src/add.js")
 const expect = chai.expect
 
 
