382057c5ec758579e45a5c058bb88bea7d55eca7
Tuija Palovuori <tuija.palovuori@tuni.fi>
2021-11-22 12:12:35 +0200

    separated defaultTo tests each under their own it()

--- test/defaultTo.test.js
+++ test/defaultTo.test.js
@@ -8,38 +8,78 @@ const expect = chai.expect
 // or undefined are still tested as well.
 
 describe("defaultTo", () => {
- it("truthy value and null/undefined/NaN default value gives value", () =>{
+
+ it("truthy value (positive number) and null default value gives value", () =>{
      expect(defaultTo(4, null)).to.equal(4);
+ });
+ it("truthy value (empty list) NaN default value gives value", () =>{
      let emptylist = [];
      expect(defaultTo(emptylist, NaN)).to.equal(emptylist);
-     let emptyobj = [];
+ });
+ it("truthy value (empty object) undefined default value gives value", () =>{
+     let emptyobj = {};
      expect(defaultTo(emptyobj, undefined)).to.equal(emptyobj);
  });
- it("null/undefined/NaN value and truthy default value gives default value", () =>{
+
+
+ it("null value and truthy default value (string) gives default value", () =>{
      expect(defaultTo(null, "default value")).to.equal("default value");
+ });
+ it("NaN value and truthy default value (string) gives default value", () =>{
      expect(defaultTo(NaN, "default value")).to.equal("default value");
+ });
+ it("undefined value and truthy default value (string) gives default value", () =>{
      expect(defaultTo(undefined, "default value")).to.equal("default value");
  });
- it("null and null/undefined/NaN default value gives default value", () =>{
+
+
+ it("null value and null default value gives default value", () =>{
      expect(defaultTo(null, null)).to.equal(null);
+ });
+ it("null value and undefined default value gives default value", () =>{
      expect(defaultTo(null, undefined)).to.equal(undefined);
+ });
+ it("null value and NaN default value gives default value", () =>{
      expect(defaultTo(null, NaN)).to.be.NaN;
  });
- it("undefined and null/undefined/NaN default value gives default value", () =>{
+
+
+ it("undefined value and null default value gives default value", () =>{
      expect(defaultTo(undefined, null)).to.equal(null);
+ });
+ it("undefined value and undefined default value gives default value", () =>{
      expect(defaultTo(undefined, undefined)).to.equal(undefined);
+ });
+ it("undefined value and NaN default value gives default value", () =>{
      expect(defaultTo(undefined, NaN)).to.be.NaN;
  });
- it("NaN and null/undefined/NaN default value gives default value", () =>{
+
+
+ it("NaN value and null default value gives default value", () =>{
      expect(defaultTo(NaN, null)).to.equal(null);
+ });
+ it("NaN value and undefined default value gives default value", () =>{
      expect(defaultTo(NaN, undefined)).to.equal(undefined);
+ });
+ it("NaN value and nNaN default value gives default value", () =>{
      expect(defaultTo(NaN, NaN)).to.be.NaN;
  });
- it("falsy values that are not NaN, null or undefined with truthy default value returns value", () =>{
+
+
+ it("falsy but non-NaN/null/undefined value (zero) with truthy default value (negative number) returns value", () =>{
      expect(defaultTo(0, -15)).to.equal(0);
+ });
+ it("falsy but non-NaN/null/undefined value (negative zero) with truthy default value (true) returns value", () =>{
      expect(defaultTo(-0, true)).to.equal(-0);
+ });
+ it("falsy but non-NaN/null/undefined value (BigInt zero) with truthy default value (string) returns value", () =>{
      expect(defaultTo(0n, "Default value")).to.equal(0n);
+ });
+ it("falsy but non-NaN/null/undefined value (empty string) with truthy default value (Date) returns value", () =>{
      expect(defaultTo("", new Date())).to.equal("");
+ });
+ it("falsy but non-NaN/null/undefined value (false) with truthy default value (Infinity) returns value", () =>{
      expect(defaultTo(false, Infinity)).to.equal(false);
  });
+
 })
