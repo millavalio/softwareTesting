c1d538d3cd8a686e51a545c090eeed3b0f54084e
Tuija Palovuori <tuija.palovuori@tuni.fi>
2021-11-24 16:46:51 +0200

    more tests for toString

--- test/toString.test.js
+++ test/toString.test.js
@@ -5,14 +5,68 @@ const expect = chai.expect
 describe("toString", () => {
 
  // Null, NaN, undefined
- it("returns \"null\" for null", () =>{
-     expect(toString(null)).to.equal("null");
+ it("returns empty string for null", () =>{
+     expect(toString(null)).to.equal("");
  });
  it("returns \"NaN\" for NaN", () =>{
      expect(toString(NaN)).to.equal("NaN");
  });
- it("returns \"undefined\" for undefined", () =>{
-     expect(toString(undefined)).to.equal("undefined");
+ it("returns empty string for undefined", () =>{
+     expect(toString(undefined)).to.equal("");
+ });
+
+ // Returns strings as is
+ it("returns empty string as is", () =>{
+     expect(toString("")).to.equal("");
+ });
+ it("returns simple string as is", () =>{
+     expect(toString("this is a string")).to.equal("this is a string");
+ });
+ it("returns more complicated string as is", () =>{
+     const longstring = "Hello world! 4 - 9 = -5. 2^3 = 8. Shouting! Questioning? The quick brown fox jumps over the lazy dog. \n\n\n !#%&|åäöüñ"
+     expect(toString(longstring)).to.equal(longstring);
+ });
+
+ // Numbers
+ it("returns positive number as string", () =>{
+     expect(toString(1.89)).to.equal("1.89");
+ });
+ it("returns negative number as string", () =>{
+     expect(toString(-54)).to.equal("-54");
+ });
+ it("returns number 0 as string", () =>{
+     expect(toString(0)).to.equal("0");
+ });
+ it("returns number -0 as string, preserves sign", () =>{
+     expect(toString(-0)).to.equal("-0");
+ });
+ it("returns infinity as string", () =>{
+     expect(toString(Infinity)).to.equal("Infinity");
+ });
+ it("returns negative infinity as string", () =>{
+     expect(toString(-Infinity)).to.equal("-Infinity");
+ });
+
+ // Booleans
+ it("returns true as string", () =>{
+     expect(toString(true)).to.equal("true");
+ });
+ it("returns false as string", () =>{
+     expect(toString(false)).to.equal("false");
+ });
+
+ // Arrays
+ it("returns empty array as empty string", () =>{
+     expect(toString([])).to.equal("");
+ });
+ it("returns array of elements as a comma separated sequence of those elements converted to string form (same type elements)", () =>{
+     expect(toString([1, 2, 3])).to.equal("1,2,3");
+ });
+ it("returns array of elements as a comma separated sequence of those elements converted to string form (different type elements)", () =>{
+     expect(toString([1, "str", null, false])).to.equal("1,str,,false");
+ });
+ it("returns array of elements as a comma separated sequence of those elements converted to string form (nested arrays)", () =>{
+     expect(toString([1, [2, [3, 4], 5], 6])).to.equal("1,2,3,4,5,6");
  });
 
  // TODO rest
